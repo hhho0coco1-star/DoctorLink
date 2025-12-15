d2f2e0c862fc700f22a118c152ccb1fee5512a3c
Me_long <hhho0coco1@gmail.com>
2025-12-15 10:06:59 +0900

    mainpage header, sidebar 구조 잡기

---
+++ src/header/MainHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "./MainHeader.css";
+
+export default function MainHeader() {
+
+    return (
+        <div>
+            {/* header */}
+            <header>
+
+            </header>
+
+            {/* aside */}
+            <aside>
+
+            </aside>
+        </div>
+    )
+}

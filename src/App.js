aa05280e31f458fb5e4974164cada2e68412cec4
meloooong <hhho0coco@gmail.com>
2025-12-22 01:48:15 +0900

    startpage 파일정리

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import MyPage from './addressTest/MyPage';
-import Calendar from './addressTest/Calendar';
-import DashBoard from './/mainPage/DL04_DashBoard';
-import MainHeader from './mainHeader/MainHeader';
 
 import StartPage from './startPage/StartPage';
 
